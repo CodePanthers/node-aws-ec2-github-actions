462c28f679b8ad6221499f44f4f493e36ce3a283
Fardeen Khan <khanfardeen9983@gmail.com>
2022-04-16 20:57:58 +0530

    Project setup

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Github Actions - Deployment Tutorial
+Deploy Node.js Express server to AWS EC2 machine using Github actions
+
+This codebase is part of a Tutorial designed by CodePanthers. 
+
+Link to the article - 
---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+app.get("/", (req,res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
