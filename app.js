e41c9c64fe3600f12f581db18d00c5e323957a67
Fardeen Khan <khanfardeen9983@gmail.com>
2022-04-17 19:21:42 +0530

    Testing Github Action

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
-    res.send("<h1>Hello World</h1>");
+    res.send("<h1>Hello World From new commit</h1>");
 });
 
 app.listen(PORT, () => {
