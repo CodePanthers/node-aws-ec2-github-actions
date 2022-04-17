e2c45f244bd91991aa951365fbc80583923e3a39
Fardeen Khan <khanfardeen9983@gmail.com>
2022-04-17 19:50:50 +0530

    Another One: Test Action

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
-    res.send("<h1>Hello World From new commit</h1>");
+    res.send("<h1>Another One</h1>");
 });
 
 app.listen(PORT, () => {
